29b04aedb5b7e5c1aea9d3efc1cdf580c239faa5
Berkeley Martinez <Berkeley@robotie.com>
2017-03-24 21:06:23 -0700

    feat(Move to ava testing and nyc coverage):

--- test/reducer.js
+++ test/reducer.js
@@ -1,4 +1,5 @@
-import expect from 'expect';
+import test from 'ava';
+
 import reducer, {
   createLayout,
   createPane,
@@ -6,10 +7,9 @@ import reducer, {
   splitTypes,
   split,
   join
-} from '../';
+} from '../src/reducers';
 
-const test = it;
-test('split right', () => {
+test('split right', (t) => {
   const endState = reducer(
     createLayout(),
     split(0, splitTypes.right)
@@ -20,24 +20,25 @@ test('split right', () => {
   const added =
     endState.panesById[parent.childIds[parent.childIds.length - 1]];
 
-  expect(parent).toExist();
-  expect(added).toExist();
-  expect(parent.childIds).toEqual([ original.id, added.id ]);
-  expect(parent.direction).toEqual(directions.row);
-  expect(original.parentId).toEqual(parent.id);
-  expect(added.parentId).toEqual(parent.id);
+  t.truthy(parent);
+  t.truthy(added);
+  t.deepEqual(parent.childIds, [ original.id, added.id ]);
+  t.is(parent.direction, directions.row);
+  t.is(original.parentId, parent.id);
+  t.is(added.parentId, parent.id);
 });
 
-test('split left', () => {
+test('split left', (t) => {
   const endState = reducer(createLayout(), split(0, splitTypes.left));
   const original = endState.panesById[0];
   const parent = endState.panesById[original.parentId];
   const added = endState.panesById[parent.childIds[0]];
-  expect(parent.childIds).toEqual([ added.id, original.id ]);
-  expect(parent.direction).toEqual(directions.row);
+
+  t.deepEqual(parent.childIds, [ added.id, original.id ]);
+  t.is(parent.direction, directions.row);
 });
 
-test('split above', () => {
+test('split above', (t) => {
   const endState = reducer(
     createLayout(),
     split(0, splitTypes.above)
@@ -46,11 +47,11 @@ test('split above', () => {
   const parent = endState.panesById[original.parentId];
   const added = endState.panesById[parent.childIds[0]];
 
-  expect(parent.childIds).toEqual([ added.id, original.id ]);
-  expect(parent.direction).toEqual(directions.col);
+  t.deepEqual(parent.childIds, [ added.id, original.id ]);
+  t.is(parent.direction, directions.col);
 });
 
-test('split below', () => {
+test('split below', (t) => {
   const endState = reducer(
     createLayout(),
     split(0, splitTypes.below)
@@ -59,11 +60,12 @@ test('split below', () => {
   const parent = endState.panesById[original.parentId];
   const added =
     endState.panesById[parent.childIds[parent.childIds.length - 1]];
-  expect(parent.childIds).toEqual([ original.id, added.id ]);
-  expect(parent.direction).toEqual(directions.col);
+
+  t.deepEqual(parent.childIds, [ original.id, added.id ]);
+  t.is(parent.direction, directions.col);
 });
 
-test('join one of two in row below root', () => {
+test('join one of two in row below root', (t) => {
   const startState = createLayout({
     rootId: 1,
     panesById: {
@@ -93,18 +95,18 @@ test('join one of two in row below root', () => {
       })
     }
   });
-
   const endState = reducer(startState, join(2, 0));
-  expect(endState.rootId).toEqual(2);
-  expect(endState.panesById[1]).toNotExist();
-  expect(endState.panesById[0]).toNotExist();
-  expect(endState.panesById[2]).toExist();
-  expect(endState.panesById[2].direction).toNotExist();
-  expect(endState.panesById[2].parent).toNotExist();
+
+  t.is(endState.rootId, 2);
+  t.falsy(endState.panesById[1]);
+  t.falsy(endState.panesById[0]);
+  t.truthy(endState.panesById[2]);
+  t.falsy(endState.panesById[2].direction);
+  t.falsy(endState.panesById[2].parent);
 });
 
 
-test('join one of three in row below root', () => {
+test('join one of three in row below root', (t) => {
   const startState = createLayout({
     rootId: 1,
     panesById: {
@@ -145,15 +147,16 @@ test('join one of three in row below root', () => {
 
   const action = join(2, 0);
   const endState = reducer(startState, action);
-  expect(endState.rootId).toEqual(1);
-  expect(endState.panesById[1]).toExist();
-  expect(endState.panesById[1].childIds).toEqual([ 2, 3 ]);
-  expect(endState.panesById[0]).toNotExist();
-  expect(endState.panesById[2]).toExist();
-  expect(endState.panesById[2].parentId).toBe(1);
+
+  t.is(endState.rootId, 1);
+  t.truthy(endState.panesById[1]);
+  t.deepEqual(endState.panesById[1].childIds, [ 2, 3 ]);
+  t.falsy(endState.panesById[0]);
+  t.truthy(endState.panesById[2]);
+  t.is(endState.panesById[2].parentId, 1);
 });
 
-test('join one of two in row below root', () => {
+test('join one of two in row below root', (t) => {
   const startState = createLayout({
     rootId: 1,
     panesById: {
@@ -202,10 +205,11 @@ test('join one of two in row below root', () => {
 
   const action = join(3, 4);
   const endState = reducer(startState, action);
-  expect(endState.panesById[2]).toNotExist();
-  expect(endState.panesById[1].childIds).toEqual([ 0, 3 ]);
-  expect(endState.panesById[3].parentId).toEqual(1);
-  expect(endState.panesById[4]).toNotExist();
-  expect(endState.panesById[3]).toExist();
-  expect(endState.panesById[3].direction).toNotExist();
+
+  t.falsy(endState.panesById[2]);
+  t.deepEqual(endState.panesById[1].childIds, [ 0, 3 ]);
+  t.is(endState.panesById[3].parentId, 1);
+  t.falsy(endState.panesById[4]);
+  t.truthy(endState.panesById[3]);
+  t.falsy(endState.panesById[3].direction);
 });
