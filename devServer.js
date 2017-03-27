dfbf05c0b48fd8d19cc48db49d53849c76e3adcb
Berkeley Martinez <Berkeley@robotie.com>
2017-03-27 15:24:33 -0700

    style(dev-server): Lint file

--- devServer.js
+++ devServer.js
@@ -1,63 +1,41 @@
-'use strict'
+const express = require('express');
+const webpack = require('webpack');
+const config = require('./webpack.config.dev');
+const compression = require('compression');
 
-let express = require('express')
-let webpack = require('webpack')
-let config = require('./webpack.config.dev')
-let compression = require('compression')
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-// let path = require('path') 
-// let requestProxy = require('express-request-proxy')
-// let objectAssign = require('object-assign')
-
-let app = express()
-let server = require('http').createServer(app)
-let io = require('socket.io')(server)
-
-let compiler = webpack(config)
-let port = 3000
+const compiler = webpack(config);
+const port = 3000;
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
-}))
+}));
 
-app.use(require('webpack-hot-middleware')(compiler))
+app.use(require('webpack-hot-middleware')(compiler));
 
 app.use(compression({
   threshold: 512
-}))
+}));
 
-app.use('/', express.static('.'))
+app.use('/', express.static('.'));
 
-// app.all('*', function(req, res, next) {
-//   let url = require('url').parse(req.url)
-//   let conf = objectAssign({}, req, {
-//     url: 'http://127.0.0.1:8888' + url.pathname,
-//     timeout: 120000
-//   })
-//   requestProxy(conf)(req, res, next)
-// })
+server.listen(port, '0.0.0.0', function(err) {
+  if (err) { throw err; }
+  console.log('Listening at http://localhost:' + port);
+});
 
-// app.get('*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'index.html'))
-// })
-
-server.listen(port, '0.0.0.0', function (err) {
-  if (err) {
-    console.log(err) // eslint-disable-line no-console
-    return
-  }
-  console.log('Listening at http://localhost:' + port) // eslint-disable-line no-console
-})
-
-io.on('connection', function (socket) {
-  io.set('origins', '*:*')
-  console.log('connected') // eslint-disable-line no-console
-  socket.emit('update', 'connected')
-  socket.on('single', function () {
-    socket.emit('update', 'single')
-  })
-  socket.on('publish', function (data) {
-    io.sockets.emit('update', data)
-  })
-})
+io.on('connection', function(socket) {
+  io.set('origins', '*:*');
+  console.log('connected');
+  socket.emit('update', 'connected');
+  socket.on('single', function() {
+    socket.emit('update', 'single');
+  });
+  socket.on('publish', function(data) {
+    io.sockets.emit('update', data);
+  });
+});
