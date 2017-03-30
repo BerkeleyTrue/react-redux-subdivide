6f04f1eb1f1ce68d210d781d0d81d4596b6d4c64
Berkeley Martinez <Berkeley@robotie.com>
2017-03-30 16:33:32 -0700

    style(webpack): lint lib file

--- webpack.config.lib.js
+++ webpack.config.lib.js
@@ -1,8 +1,4 @@
-'use strict'
-
-let path = require('path')
-
-//let ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path');
 
 module.exports = {
   output: {
@@ -10,9 +6,6 @@ module.exports = {
     path: path.join(__dirname, 'lib'),
     publicPath: '/lib/'
   },
-//  plugins: [
-//    new ExtractTextPlugin(path.parse(process.argv[2]).name + '.css')
-//  ],
   module: {
     loaders: [
       {
@@ -21,4 +14,4 @@ module.exports = {
       }
     ]
   }
-}
+};
